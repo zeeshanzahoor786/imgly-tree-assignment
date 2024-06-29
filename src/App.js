cd04d7aceb7d03d5baaa5f9760867750ce19f24d
zeeshan zahoor <105010948+zeeshanzahoor786@users.noreply.github.com>
2024-06-29 15:19:38 +0200

    part1: Fetch and display the data-completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import TreeComponent from './TreeComponent';
 function App() {
   return (
--- src/TreeComponent.js
+++ src/TreeComponent.js
@@ -1,9 +1,38 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 const TreeComponent = () => {
+  const [data, setData] = useState([]);
+
+  // Fetch the initial tree data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://ubique.img.ly/frontend-tha/data.json');
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Render the tree recursively
+  const renderTree = (nodes) => (
+    <ul>
+      {nodes.map((node) => (
+        <li>
+          {node.label}
+          {node.children && renderTree(node.children)}
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <div>
-      starting imgly assignment
+      <h1>Tree Structure</h1>
+      {renderTree(data)}
     </div>
   );
 };
